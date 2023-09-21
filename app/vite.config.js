d3e6cfea33b5181d1c299a633d77d1207fd009e9
benspector-mls <ben@marcylabschool.org>
2023-09-21 16:27:11 -0400

    rebuilding and fixing rename issues

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
   build: { outDir: '..' },
-  base: '/MealTime-Day-4/', // needed for github pages just put the repo name here
+  base: '/MealTime-Final/', // needed for github pages just put the repo name here
   emptyOutDir: true, // this will clear the dist folder before rebuilding
 });
